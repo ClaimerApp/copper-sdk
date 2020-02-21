993d34d1089e5ed132a84724b17b9072cd983545
Adam McCann <adam@mccann.io>
2020-02-21 20:58:16 +0000

    Fix args

--- copper_sdk/customer_sources.py
+++ copper_sdk/customer_sources.py
@@ -2,5 +2,5 @@ class CustomerSources():
     def __init__(self, copper):
         self.copper = copper
 
-    def get(self, id):
+    def get(self):
         return self.copper.get(f'/customer_sources')
--- copper_sdk/loss_reasons.py
+++ copper_sdk/loss_reasons.py
@@ -2,5 +2,5 @@ class LossReasons():
     def __init__(self, copper):
         self.copper = copper
 
-    def get(self, id):
+    def get(self):
         return self.copper.get(f'/loss_reasons')
